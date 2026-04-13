5dc15af56a79d09f600733ecbe1deccbaeb87156
IronLab Deploy <josh@tcr.design>
2026-04-13 11:49:04 +0000

    Force new asset filenames to bust Safari cache

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/ironlab-[hash].js`,
+        chunkFileNames: `assets/ironlab-chunk-[hash].js`,
+        assetFileNames: `assets/ironlab-[hash].[ext]`,
+      }
+    }
+  }
 })
